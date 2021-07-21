63cb13c83b10b2cf11cfadcd85070030a0a4f14d
Alec Tecson <tecsonalr@gmail.com>
2021-07-21 17:13:46 +0800

    Classes and Namespaces

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpPractice.Classes;
 
 namespace CSharpPractice
 {
@@ -11,16 +12,25 @@ namespace CSharpPractice
         private static double numberTwo = 12.34;
         static void Main(string[] args)
         {
-            if (numberTwo == 1)
-            {
-                var numberThree = 12f;
-            }
+            //if (numberTwo == 1)
+            //{
+            //    var numberThree = 12f;
+            //}
 
-            var numberOne = 23.45;
-            Console.WriteLine(numberOne);
-            string message = Console.ReadLine();
-            Console.WriteLine(message);
+            //var numberOne = 23.45;
+            //Console.WriteLine(numberOne);
+             
+            //Console.WriteLine(SimpleMath.Add(numberOne, numberTwo));
+
             Console.ReadLine();
+        }
+    }
+
+    class SimpleMath
+    {
+        public static double Add(double number1, double number2)
+        {
+            return number1 + number2;
         }
     }
 }
