a2a5716a3f81e54617bce66e57cc4db068ec2845
Alec Tecson <tecsonalr@gmail.com>
2021-07-21 16:57:26 +0800

    Variable and Scope

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -8,9 +8,16 @@ namespace CSharpPractice
 {
     class Program
     {
+        private static double numberTwo = 12.34;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (numberTwo == 1)
+            {
+                var numberThree = 12f;
+            }
+
+            var numberOne = 23.45;
+            Console.WriteLine(numberOne);
             string message = Console.ReadLine();
             Console.WriteLine(message);
             Console.ReadLine();
