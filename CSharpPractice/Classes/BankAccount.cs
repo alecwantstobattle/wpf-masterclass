2d6bb2bb90d2398837aee55c1088e2b0a83652fc
Alec Tecson <tecsonalr@gmail.com>
2021-07-22 15:37:36 +0800

    Method Overloading

--- CSharpPractice/Classes/BankAccount.cs
+++ CSharpPractice/Classes/BankAccount.cs
@@ -33,6 +33,11 @@ namespace CSharpPractice.Classes
             Balance = 100;
         }
 
+        public BankAccount(double initialBalance)
+        {
+            Balance += initialBalance;
+        }
+
         public double AddToBalance(double balanceToBeAdded)
         {   
             Balance += balanceToBeAdded;
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -12,6 +12,10 @@ namespace CSharpPractice
         private static double numberTwo = 12.34;
         static void Main(string[] args)
         {
+            double[] numbers = new double[] {1, 2, 3, 42, 42154};
+            var result = SimpleMath.Add(numbers);
+            Console.WriteLine(result);
+
             BankAccount bankAccount = new BankAccount();
             bankAccount.AddToBalance(100);
             Console.WriteLine(bankAccount.Balance);
@@ -29,6 +33,17 @@ namespace CSharpPractice
         public static double Add(double number1, double number2)
         {
             return number1 + number2;
+        }
+
+        public static double Add(double[] numbers)
+        {
+            double result = 0;
+            foreach (double d in numbers)
+            {
+                result += d;
+            }
+
+            return result;
         }
     }
 }
