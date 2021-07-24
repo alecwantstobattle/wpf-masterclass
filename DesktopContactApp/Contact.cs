1dd228477f8bdf514bd6a59b223409cca709d2f3
Alec Tecson <tecsonalr@gmail.com>
2021-07-24 18:38:29 +0800

    The ToString method

--- DesktopContactApp/Contact.cs
+++ DesktopContactApp/Contact.cs
@@ -14,5 +14,10 @@ namespace DesktopContactApp
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
     }
 }
