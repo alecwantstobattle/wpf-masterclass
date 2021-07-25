7934d15f69df1b0d73df8f9bf67b5ef55c22e383
Alec Tecson <tecsonalr@gmail.com>
2021-07-25 17:23:46 +0800

    Binding to a Custom Control

--- DesktopContactApp/Controls/ContactControl.xaml.cs
+++ DesktopContactApp/Controls/ContactControl.xaml.cs
@@ -20,17 +20,24 @@ namespace DesktopContactApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact contact;
-
         public Contact Contact
         {
-            get { return contact; }
-            set
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact. This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty = DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl),new PropertyMetadata(new Contact() { Name = "Name Lastname", Phone = "(123) 456 7890", Email = "example@domain.com"}, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+
+            if (control != null)
             {
-                contact = value;
-                nameTextBlock.Text = contact.Name;
-                emailTextBlock.Text = contact.Email;
-                phoneTextBlock.Text = contact.Phone;
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
             }
         }
 
