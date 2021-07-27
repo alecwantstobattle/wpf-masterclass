e9b6442cfabe63b4c08a25edfca7e9900aeac50b
Alec Tecson <tecsonalr@gmail.com>
2021-07-27 21:21:02 +0800

    Requests to the AccuWeather API

---
+++ WeatherApp/ViewModel/AccuWeatherAPI.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    class AccuWeatherAPI
+    {
+        public const string API_KEY = "";
+        public const string BASE_URL = "http://dataservice.accuweather/forecasts/v1/daily/5day/{0}?apikey={0}&metric=true";
+
+        public async Task<AccuWeather> GetWeatherInformationAsync(string locationKey)
+        {
+            AccuWeather result = new AccuWeather();
+
+            string url = string.Format(BASE_URL, locationKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                result = JsonConvert.DeserializeObject<AccuWeather>(json);
+            }
+
+            return result;
+        }
+    }
+}
